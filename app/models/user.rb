c3a3fe045e8ac9d5fb0966e5b815ef284119a13e
harunauchida <sayos8891@gmail.com>
2025-10-06 11:08:14 +0900

    アイコンの表示などの修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ApplicationRecord
+  has_one_attached :icon   # ←これでユーザーごとに1枚のアイコンを保存できる
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
